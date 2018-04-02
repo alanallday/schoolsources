acb29739a8ac8f15a690ef570bbb0e1f56adb355
alanallday <30965931+alanallday@users.noreply.github.com>
2018-04-01 19:32:21 -0700

    edits

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
 	  	<link rel="stylesheet" href="stylesheets/style.css">
 		<html lang="en" xml:lang="fr" xmlns="http://www.w3.org/1999/xhtml">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
 	</head>
-	<body class="home-page" onload="document.body.className += ' loaded';">
+	<body class="home" onload="document.body.className += ' loaded';">
 		<?php include 'partials/header.php'; ?>
 		<main>
 			<?php include 'partials/homepage.php'; ?>
