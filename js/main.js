b090bb0af6b5dc811af4e063a48d815148f4cde7
alanallday <30965931+alanallday@users.noreply.github.com>
2018-11-15 11:35:32 -0800

    hello

--- js/main.js
+++ js/main.js
@@ -414,7 +414,9 @@ function courseHeader() {
 		$('.course-block.hero a').click(function(e) {
 			e.preventDefault();
 			var $link = $(this).attr('href');
-			$("html, body").animate({ scrollTop: $($link).offset().top - 100}, 500);
+			$("html, body").stop(true).animate({
+            	scrollTop: $($link).offset().top - 100
+        	}, 500);
 		});
 	}
 }
